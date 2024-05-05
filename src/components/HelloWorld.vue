075632938b3303782c5c179671f828a264433753
sherbek turdiyev <sherbekturdiyev98@gmail.com>
2024-05-05 14:01:41 +0500

    daily and all flexible

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,82 +1,103 @@
 <template>
-  <div class="flex justify-between">
+  <div class="">
     <el-card class="box-card w-full">
       <template #header>
-        <div class="card-header flex justify-between">
-          <div class="text-3xl w-2/12 my-auto ml-10">
-            <div>{{ cityName }}</div>
-          </div>
-          <div class="text-2xl my-3 w-2/12 text-black">
-            <span class="mx-10 w-11/12">Hududlar</span>
-          </div>
-        </div>
+        <el-row class="card-header">
+          <el-col :xl="12" :lg="12" :md="12" class="text-3xl">
+            <div class="mx-1">{{ cityName }}</div>
+          </el-col>
+          <el-col :xl="12" :lg="12" :md="12" class="text-2xl flex justify-end">
+            <span class="mx-1">Hududlar</span>
+          </el-col>
+        </el-row>
       </template>
 
-      <div class="flex justify-between">
-        <div class="w-10/12">
-          <div class="flex justify-between first__chapter">
-            <div class="date-temp w-8/12">
-              <div class="text-2xl">{{ date }}</div>
-              <div class="text-7xl ml-7 mt-2 mr-2 font-sans flex text-blue-700">
-                <span>{{ todayWeather }}°C</span>
-                <span class="text-3xl m-9">{{desc}}</span>
+      <el-row class="">
+        <el-col :xl="20" :lg="20" :md="20" class="">
+          <el-row class="px-1.5">
+            <el-col :xl="17" :lg="17" :md="17" class="">
+              <div class="text-2xl border-solid border-2 border-sky-500 rounded"
+                style="width: max-content"
+              >{{ date }}</div>
+              <div class="text-7xl my-2 font-sans flex items-end text-blue-700">
+                <span>{{ todayWeather.temp }}°C</span>
+                <span class="text-3xl ml-2">{{todayWeather.main}}</span>
               </div>
-            </div>
-            <div class="image__style w-3/12 flex">
-              <img :src="imgWeather" alt="image" width="150" >
-              <div class="">
-                <div>Namlik: {{humidity}}</div>
+            </el-col>
+            <el-col :xl="7" :lg="7" :md="7" class="bg__style p-1 rounded flex">
+              <img :src="imgUrlF+todayWeather.icon+imgUrlL" alt="image" size="185px" >
+              <div class="items-center my-auto ml-auto py-0.5 px-2 bg-orange-200 rounded">
+                <div>namlik: {{todayWeather.humidity}}</div>
+                <div>{{todayWeather.description}}</div>
               </div>
-            </div>
-          </div>
+            </el-col>
+          </el-row>
+
           <div class="sec__chapter">
-            <div class="grid gap-1 grid-cols-8">
-
-              <div class="gap__item">
-                <div class="">
-                  <div class="text-red-500 flex justify-end">
-                    <span class="mr-1">namlik:</span><span>{{humidity}}</span>
+            <el-row class="">
+              <el-col :xl="3" :lg="3" :md="3" :sm="6" v-for="item in hourly">
+                <div class="gap__item m-0.5">
+                  <div class="">
+                    <div class="text-red-500 flex justify-end">
+                      <span class="mr-1">namlik:</span><span>{{item.humidity}}</span>
+                    </div>
+                    <img :src="imgUrlF+item.icon+imgUrlL" alt="image" size="80">
                   </div>
-                  <img :src="imgWeather" alt="image" width="80">
-                </div>
-                <div class="flex justify-between">
-                  <div class="p-0.5">{{ todayWeather }}°C</div>
-                  <span class="el-text--small bg-white p-0.5 rounded ">{{clock}}</span>
-                </div>
-              </div>
-
-              <div class="gap__item" v-for="item in hourly">
-                <div class="">
-                  <div class="text-red-500 flex justify-end">
-                    <span class="mr-1">namlik:</span><span>{{item.humidity}}</span>
+                  <div class="flex justify-between">
+                    <div class="p-0.5">{{ item.temp }}°C</div>
+                    <span class="el-text--small bg-white p-0.5 rounded ">{{item.hour}}</span>
                   </div>
+                </div>
+              </el-col>
+            </el-row>
+          </div>
+
+          <el-row>
+            <el-col :xl="12" :lg="12" :md="12" :sm="24" v-for="item in daily">
+              <el-row class="three__chapter flex">
+                <el-col :xl="8" :lg="8" :md="8" :sm="8" class="w-4/12">
                   <img :src="imgUrlF+item.icon+imgUrlL" alt="image" width="80">
-                </div>
-                <div class="flex justify-between">
-                  <div class="p-0.5">{{ item.temp }}°C</div>
-                  <span class="el-text--small bg-white p-0.5 rounded ">{{item.hour}}</span>
-                </div>
-              </div>
-
+                  <div>{{item.description}}</div>
+                </el-col>
+                <el-col :xl="8" :lg="8" :md="8" :sm="8" class="my-auto w-4/12">
+                  <div>
+                    <span class="mr-0.5">max harorat:</span>
+                    <span class=" text-red-500">{{item.temp}}°C</span>
+                  </div>
+                  <div class="flex">
+                    <span class="mr-0.5">namlik:</span>
+                    <span class=" text-blue-500">{{item.humidity}}</span>
+                  </div>
+                </el-col>
+                <el-col :xl="8" :lg="8" :md="8" :sm="8" class="item-center flex flex-wrap w-4/12">
+                  <div class="m-1 p-0.5 bg-orange-200 rounded text-black-500" style="width: -webkit-fill-available;">
+                    <span class="mr-0.5">kunduzgi harorat:</span>
+                    <span>{{item.tempDay}}°C</span>
+                  </div>
+                  <div class="m-1 p-0.5 bg-black rounded text-white" style="width: -webkit-fill-available;">
+                    <span class="mr-0.5">kechasi harorat:</span>
+                    <span>{{item.tempNight}}°C</span>
+                  </div>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+        </el-col>
+        <el-col :xl="4" :lg="4" :md="4" class="">
+          <div v-for="(item, index) in County" :key="index">
+            <div>
+              <el-button
+                type="success"
+                style="width: -webkit-fill-available;"
+                @click="getItemWeather(item)"
+                class="my-1 mx-2 w-100 text-black bg-white"
+              >
+                {{ item.name }}
+              </el-button>
             </div>
           </div>
-          <div class="flex justify-between three__chapter">
-            <div>4 kun</div>
-            <div>4kun</div>
-          </div>
-        </div>
-        <div class="w-2/12">
-          <div v-for="(item, index) in County" :key="index">
-            <el-button
-              type="success"
-              @click="getItemWeather(item)"
-              class="my-1 mx-2 w-11/12 text-black bg-white"
-            >
-              {{ item.name }}
-            </el-button>
-          </div>
-        </div>
-      </div>
+        </el-col>
+      </el-row>
 
       <template #footer>
         <div class="flex justify-between my-3">
@@ -122,7 +143,6 @@ let County = reactive([
 
 let cityName = ref('Hudud nomi');
 let todayWeather = reactive({});
-let desc = ref('');
 let date = ref('');
 let humidity = ref('');
 let icon = ref('');
@@ -161,65 +181,133 @@ async function getItemWeather(item) {
         `data/2.8/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&` +
           `appid=9dd86907fe501cec50da3d087e4e9dc0&units=metric&lang=uz`
       )
-      todayWeather = res.data.current.temp;
+      todayWeather = {
+        temp: res.data.current.temp,
+        humidity: res.data.current.humidity,
+        icon: res.data.current.weather[0].icon,
+        main: res.data.current.weather[0].main,
+        description: res.data.current.weather[0].description
+      };
     // hourly
       hourly = [
+        {
+          humidity: res.data.hourly[0].humidity,
+          temp: res.data.hourly[0].temp,
+          icon: res.data.hourly[0].weather[0].icon,
+          hour: (soat+1) % 24 +":"+ minut
+        },
+        {
+          humidity: res.data.hourly[1].humidity,
+          temp: res.data.hourly[1].temp,
+          icon: res.data.hourly[1].weather[0].icon,
+          hour: (soat+2) % 24 +":"+ minut
+        },
         {
           humidity: res.data.hourly[2].humidity,
           temp: res.data.hourly[2].temp,
           icon: res.data.hourly[2].weather[0].icon,
-          hour: (soat+1) % 24 +":"+ minut
+          hour: (soat+3) % 24 +":"+ minut
         },
         {
           humidity: res.data.hourly[3].humidity,
           temp: res.data.hourly[3].temp,
           icon: res.data.hourly[3].weather[0].icon,
-          hour: (soat+2) % 24 +":"+ minut
+          hour: (soat+4) % 24 +":"+ minut
         },
         {
           humidity: res.data.hourly[4].humidity,
           temp: res.data.hourly[4].temp,
           icon: res.data.hourly[4].weather[0].icon,
-          hour: (soat+3) % 24 +":"+ minut
+          hour: (soat+5) % 24 +":"+ minut
         },
         {
           humidity: res.data.hourly[5].humidity,
           temp: res.data.hourly[5].temp,
           icon: res.data.hourly[5].weather[0].icon,
-          hour: (soat+4) % 24 +":"+ minut
+          hour: (soat+6) % 24 +":"+ minut
         },
         {
           humidity: res.data.hourly[6].humidity,
           temp: res.data.hourly[6].temp,
           icon: res.data.hourly[6].weather[0].icon,
-          hour: (soat+5) % 24 +":"+ minut
+          hour: (soat+7) % 24 +":"+ minut
         },
         {
           humidity: res.data.hourly[7].humidity,
           temp: res.data.hourly[7].temp,
           icon: res.data.hourly[7].weather[0].icon,
-          hour: (soat+6) % 24 +":"+ minut
-        },
-        {
-          humidity: res.data.hourly[8].humidity,
-          temp: res.data.hourly[8].temp,
-          icon: res.data.hourly[8].weather[0].icon,
           hour: (soat+7) % 24 +":"+ minut
         },
       ];
     // daily
     daily = [
-        res.data.daily,
-    console.log(res.data.daily)
+      {
+        humidity: res.data.daily[0].humidity,
+        temp: res.data.daily[0].temp.max,
+        tempDay: res.data.daily[0].temp.day,
+        tempNight: res.data.daily[0].temp.night,
+        icon: res.data.daily[0].weather[0].icon,
+        description: res.data.daily[0].weather[0].description
+      },
+      {
+        humidity: res.data.daily[1].humidity,
+        temp: res.data.daily[1].temp.max,
+        tempDay: res.data.daily[1].temp.day,
+        tempNight: res.data.daily[1].temp.night,
+        icon: res.data.daily[1].weather[0].icon,
+        description: res.data.daily[1].weather[0].description
+      },
+      {
+        humidity: res.data.daily[2].humidity,
+        temp: res.data.daily[2].temp.max,
+        tempDay: res.data.daily[2].temp.day,
+        tempNight: res.data.daily[2].temp.night,
+        icon: res.data.daily[2].weather[0].icon,
+        description: res.data.daily[2].weather[0].description
+      },
+      {
+        humidity: res.data.daily[3].humidity,
+        temp: res.data.daily[3].temp.max,
+        tempDay: res.data.daily[3].temp.day,
+        tempNight: res.data.daily[3].temp.night,
+        icon: res.data.daily[3].weather[0].icon,
+        description: res.data.daily[3].weather[0].description
+      },
+      {
+        humidity: res.data.daily[4].humidity,
+        temp: res.data.daily[4].temp.max,
+        tempDay: res.data.daily[4].temp.day,
+        tempNight: res.data.daily[4].temp.night,
+        icon: res.data.daily[4].weather[0].icon,
+        description: res.data.daily[4].weather[0].description
+      },
+      {
+        humidity: res.data.daily[5].humidity,
+        temp: res.data.daily[5].temp.max,
+        tempDay: res.data.daily[5].temp.day,
+        tempNight: res.data.daily[5].temp.night,
+        icon: res.data.daily[5].weather[0].icon,
+        description: res.data.daily[5].weather[0].description
+      },
+      {
+        humidity: res.data.daily[6].humidity,
+        temp: res.data.daily[6].temp.max,
+        tempDay: res.data.daily[6].temp.day,
+        tempNight: res.data.daily[6].temp.night,
+        icon: res.data.daily[6].weather[0].icon,
+        description: res.data.daily[6].weather[0].description
+      },
+      {
+        humidity: res.data.daily[7].humidity,
+        temp: res.data.daily[7].temp.max,
+        tempDay: res.data.daily[7].temp.day,
+        tempNight: res.data.daily[7].temp.night,
+        icon: res.data.daily[7].weather[0].icon,
+        description: res.data.daily[7].weather[0].description
+      }
     ];
 
     if (!item){cityName.value = res.data.timezone;}
-      humidity.value = res.data.current.humidity;
-      icon.value = res.data.current.weather[0].icon;
-      desc.value = res.data.current.weather[0].main;
-      getImgUrl(icon.value).then(url => {
-        imgWeather.value = url;
-      });
   } catch (e) {
     console.log(e)
   }
@@ -254,7 +342,7 @@ async function getImgUrl(value) {
 }
 .gap__item{
   border-radius: 7px;
-  padding: 7px;
+  padding: 5px;
   //height: 120px;
   background-color: #e4eaf6;
 
@@ -264,17 +352,17 @@ async function getImgUrl(value) {
   border-radius: 7px;
   padding: 5px;
   background-color: #e4eaf6;
-  height: 170px;
+  //height: 170px;
 }
 .date-temp {
   margin: 7px;
   border-radius: 7px;
   padding: 5px;
 }
-.image__style {
-  margin: 7px;
-  border-radius: 7px;
-  padding: 5px;
+.bg__style {
+  //margin: 7px;
+  //border-radius: 7px;
+  //padding: 5px;
   background-color: #e4eaf6;
 }
 .el-card__header {
