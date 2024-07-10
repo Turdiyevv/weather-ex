f00e5cf47770b867e85e299f7830723c6ceeae23
sherbek turdiyev <sherbekturdiyev98@gmail.com>
2024-07-11 02:58:25 +0500

    tz first

--- src/components/actionPage.vue
+++ src/components/actionPage.vue
@@ -1,7 +1,46 @@
 <template>
   <div class="">
-    <el-card class="mb-1">
-      div
+    <el-card class="my-4 mx-2">
+  <el-form
+    ref="formRef"
+    style="max-width: 600px"
+    :model="numberValidateForm"
+    label-width="auto"
+    class="demo-ruleForm"
+  >
+    <el-form-item
+      label="number"
+      prop="number"
+      :rules="[
+        { required: true, message: 'number is required' },
+        { type: 'number', message: 'number must be a number' },
+      ]"
+    >
+      <el-input
+        v-model.number="numberValidateForm.number"
+        type="text"
+        autocomplete="off"
+      />
+
+    </el-form-item>
+    <el-form-item
+      label="email"
+      prop="email"
+      :rules="[
+        { required: true, message: 'email is required' },
+        { type: 'email', message: 'email must be a email' },
+      ]"
+    >
+      <el-input
+        v-model.number="numberValidateForm.email"
+        type="text"
+        autocomplete="off"
+      />
+    </el-form-item>
+    <el-form-item class="ml-14">
+      <el-button @click="submitForm(formRef)">Submit</el-button>
+    </el-form-item>
+  </el-form>
     </el-card>
   </div>
 </template>
@@ -10,278 +49,34 @@ import {onMounted, reactive, ref} from 'vue'
 import http from '@/plugins/axios.js'
 import axios from 'axios'
 import {createLogger} from "vite";
-import {ElNotification} from "element-plus";
+import { MaskInput } from 'vue-3-mask';
+import {ElNotification, FormInstance} from "element-plus";
 
-let devDialog = ref(false)
-let password = ref('')
-let showData = ref(false)
-const open1 = () => {
-  ElNotification({
-    title: 'Success',
-    message: 'Developer bo\'limi muvaffaqiyatli ochildi' ,
-    type: 'success',
+const formRef = ref<FormInstance>();
+const numberValidateForm = reactive({
+  number: '',
+  email: '',
+});
+const submitForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  formEl.validate((valid) => {
+    if (valid) {
+      console.log(numberValidateForm.number, numberValidateForm.email)
+      ElNotification.success({
+        title: 'done',
+        message: 'submit !',
+        showClose: true
+      })
+    } else {
+      ElNotification.warning({
+        title: 'warning',
+        message: 'no active !',
+        showClose: true
+      })
+    }
   })
 }
-const open4 = () => {
-  ElNotification({
-    title: 'Error',
-    message: 'Xatolik yuz berdi developerga murojaat qiling!',
-    type: 'error',
-  })
-}
-
-let loading = ref(false)
-let County = reactive([
-  { name: 'Andijon', lat: 40.7833471, lon: 72.3506746 },
-  { name: 'Buxoro', lat: 39.7675529, lon: 64.4231326 },
-  { name: "Farg'ona", lat: 40.3764879, lon: 71.7913193 },
-  { name: 'Jizzax', lat: 40.1331797, lon: 67.8234081 },
-  { name: 'Namangan', lat: 40.9996482, lon: 71.6726238 },
-  { name: 'Qashqadaryo', lat: 38.9248284, lon: 65.7538819 },
-  { name: "Qoraqalpog'iston", lat: 44.7684079, lon: 56.1936645 },
-  { name: 'Sirdaryo', lat: 40.8309135, lon: 68.6661865 },
-  { name: 'Samarqand', lat: 39.6550017, lon: 66.9756954 },
-  { name: 'Surxondaryo', lat: 37.9409, lon: 67.5709 },
-  { name: 'Tashkent', lat: 41.3123363, lon: 69.2787079 },
-  { name: 'Xorazm', lat: 41.3565, lon: 60.8567 },
-  { name: 'Yakkabog\' tumani', lat: 38.88220587, lon: 66.82707718 }
-])
-
-let cityName = ref('Hudud nomi');
-let date = ref('');
-let lat = ref();
-let lon = ref();
-let clock = ref('');
-
-let todayWeather = ref({});
-let hourly = ref([]);
-let daily = ref([]);
-
-let soat = ref('');
-let minut = ref('');
-
-function devFunction(){
-  devDialog.value = !devDialog.value;
-}
-function checkInputPassword(){
-  if (password.value && password.value === '123'){
-    open1();
-    showData.value = true;
-  }else {
-    open4();
-  }
-  devDialog.value = false;
-  password.value = '';
-}
-
-async function getItemWeather(item) {
-  try {
-    // loading = true;
-    if (item){ lat = item.lat; lon = item.lon; cityName.value = item.name;}
-    else { lat = 38.88220587; lon = 66.82707718; cityName.value = 'Yakkabog\' tumani'}
-
-    const res = await http.get(
-      `data/2.8/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&` +
-        `appid=9dd86907fe501cec50da3d087e4e9dc0&units=metric&lang=uz`
-    ).then(res=>{
-      todayWeather.value = {
-          temp: res.data.current.temp,
-          humidity: res.data.current.humidity,
-          icon: res.data.current.weather[0].icon,
-          main: res.data.current.weather[0].main,
-          description: res.data.current.weather[0].description
-        };
-      // hourly
-      hourly.value = [
-          {
-            humidity: res.data.hourly[0].humidity,
-            temp: res.data.hourly[0].temp,
-            icon: res.data.hourly[0].weather[0].icon,
-            hour: (soat+0) % 24 +":"+ minut
-          },
-          {
-            humidity: res.data.hourly[1].humidity,
-            temp: res.data.hourly[1].temp,
-            icon: res.data.hourly[1].weather[0].icon,
-            hour: (soat+1) % 24 +":"+ minut
-          },
-          {
-            humidity: res.data.hourly[2].humidity,
-            temp: res.data.hourly[2].temp,
-            icon: res.data.hourly[2].weather[0].icon,
-            hour: (soat+2) % 24 +":"+ minut
-          },
-          {
-            humidity: res.data.hourly[3].humidity,
-            temp: res.data.hourly[3].temp,
-            icon: res.data.hourly[3].weather[0].icon,
-            hour: (soat+3) % 24 +":"+ minut
-          },
-          {
-            humidity: res.data.hourly[4].humidity,
-            temp: res.data.hourly[4].temp,
-            icon: res.data.hourly[4].weather[0].icon,
-            hour: (soat+4) % 24 +":"+ minut
-          },
-          {
-            humidity: res.data.hourly[5].humidity,
-            temp: res.data.hourly[5].temp,
-            icon: res.data.hourly[5].weather[0].icon,
-            hour: (soat+5) % 24 +":"+ minut
-          },
-          {
-            humidity: res.data.hourly[6].humidity,
-            temp: res.data.hourly[6].temp,
-            icon: res.data.hourly[6].weather[0].icon,
-            hour: (soat+6) % 24 +":"+ minut
-          },
-          {
-            humidity: res.data.hourly[7].humidity,
-            temp: res.data.hourly[7].temp,
-            icon: res.data.hourly[7].weather[0].icon,
-            hour: (soat+7) % 24 +":"+ minut
-          },
-        ];
-      // daily
-      daily.value = [
-      {
-        humidity: res.data.daily[0].humidity,
-        temp: res.data.daily[0].temp.max,
-        tempDay: res.data.daily[0].temp.day,
-        tempNight: res.data.daily[0].temp.night,
-        icon: res.data.daily[0].weather[0].icon,
-        description: res.data.daily[0].weather[0].description
-      },
-      {
-        humidity: res.data.daily[1].humidity,
-        temp: res.data.daily[1].temp.max,
-        tempDay: res.data.daily[1].temp.day,
-        tempNight: res.data.daily[1].temp.night,
-        icon: res.data.daily[1].weather[0].icon,
-        description: res.data.daily[1].weather[0].description
-      },
-      {
-        humidity: res.data.daily[2].humidity,
-        temp: res.data.daily[2].temp.max,
-        tempDay: res.data.daily[2].temp.day,
-        tempNight: res.data.daily[2].temp.night,
-        icon: res.data.daily[2].weather[0].icon,
-        description: res.data.daily[2].weather[0].description
-      },
-      {
-        humidity: res.data.daily[3].humidity,
-        temp: res.data.daily[3].temp.max,
-        tempDay: res.data.daily[3].temp.day,
-        tempNight: res.data.daily[3].temp.night,
-        icon: res.data.daily[3].weather[0].icon,
-        description: res.data.daily[3].weather[0].description
-      },
-      {
-        humidity: res.data.daily[4].humidity,
-        temp: res.data.daily[4].temp.max,
-        tempDay: res.data.daily[4].temp.day,
-        tempNight: res.data.daily[4].temp.night,
-        icon: res.data.daily[4].weather[0].icon,
-        description: res.data.daily[4].weather[0].description
-      },
-      {
-        humidity: res.data.daily[5].humidity,
-        temp: res.data.daily[5].temp.max,
-        tempDay: res.data.daily[5].temp.day,
-        tempNight: res.data.daily[5].temp.night,
-        icon: res.data.daily[5].weather[0].icon,
-        description: res.data.daily[5].weather[0].description
-      },
-      {
-        humidity: res.data.daily[6].humidity,
-        temp: res.data.daily[6].temp.max,
-        tempDay: res.data.daily[6].temp.day,
-        tempNight: res.data.daily[6].temp.night,
-        icon: res.data.daily[6].weather[0].icon,
-        description: res.data.daily[6].weather[0].description
-      },
-      {
-        humidity: res.data.daily[7].humidity,
-        temp: res.data.daily[7].temp.max,
-        tempDay: res.data.daily[7].temp.day,
-        tempNight: res.data.daily[7].temp.night,
-        icon: res.data.daily[7].weather[0].icon,
-        description: res.data.daily[7].weather[0].description
-      }
-    ];
-      // loading = false;
-    })
-  } catch (e) {
-    return alert(e)
-  }
-}
-getItemWeather();
-
-function getHozirgiSana() {
-    const hozirgiSana = new Date();
-    const kun = hozirgiSana.getDate();
-    const oy = hozirgiSana.getMonth() + 1; // Oylar 0 dan boshlanadi, shuning uchun 1 qo'shib chiqamiz
-    const yil = hozirgiSana.getFullYear();
-    soat = hozirgiSana.getHours();
-    minut = hozirgiSana.getMinutes();
-    clock = soat + ':'+ minut;
-
-    // const soatFormat = soat >= 12 ? 'PM' : 'AM'; //soatni 12 likka o'zgartirish
-    // soat = soat % 12 || 12;
-
-    return (date.value = `${kun}-${oy}-${yil} ${soat}:${minut}`);
-}
-getHozirgiSana();
-
-
-const imgUrlF = ref('https://openweathermap.org/img/wn/');
-const imgUrlL = ref('@2x.png');
 
 </script>
-<style>
-.first__chapter {
-  height: 180px;
-}
-.sec__chapter {
-  font-size: 10px;
-  margin: 7px;
-}
-.gap__item{
-  border-radius: 7px;
-  padding: 5px;
-  //height: 120px;
-  background-color: #88c0fd;
-
-}
-.three__chapter {
-  margin: 7px;
-  border-radius: 7px;
-  padding: 5px;
-  background-color: #c2dffd;
-  //height: 170px;
-}
-.date-temp {
-  margin: 7px;
-  border-radius: 7px;
-  padding: 5px;
-}
-.bg__style {
-  background-color: #598df6;
-}
-.el-card__header {
-  padding: 0;
-  margin: 5px 5px;
-  height: 90px;
-}
-.el-card__body {
-  //height: 504px;
-  padding: 0;
-  margin: 0 5px;
-  border-radius: 5px;
-}
-.el-card__footer {
-  padding: 0;
-  margin: 0 5px;
-  height: 70px;
-}
+<style scoped>
 </style>
--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import http from "@/plugins/axios.js";
 import ElementPlus from 'element-plus';
+import VueTheMask from 'vue-the-mask'
 
 const app = createApp(App);
 app.use(ElementPlus);
 app.config.globalProperties.$http = http;
 
+app.use(VueTheMask);
 app.use(createPinia());
 app.mount('#app');
